f5921453807a659b7cd68e3610f276e2e1c8cac0
Bhavin789 <bhavib.789@gmail.com>
2020-12-27 17:10:18 +0530

    css: Update css for the first page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div id="main">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="title">Dr. Norman Borlaug</h1>
+        <p>The man who saved a billion lives</p>
+
+        <div id="img-div">
+          <img
+            id="image"
+            src="https://c2.staticflickr.com/4/3689/10613180113_fdf7bcd316_b.jpg"
+            alt="Dr. Norman Borlaug image"
+          ></img>
+          <div id="img-caption">
+            Dr. Norman Borlaug, third from the left, trains biologists in Mexico
+            on how to increase wheat yields - part of his life-long war on
+            hunger.
+          </div>
+        </div>
+
+        <section id="tribute-info">
+          <h3 id="headline">Here's a time line of Dr. Borlaug's life:</h3>
+          <ul id="list">
+            <li>
+              <b>1914</b> - Born in Cresco, Iowa
+            </li>
+            <li>
+              <b>1914</b> - Leaves his family's farm to attend the University of
+              Minnesota, thanks to a Depression era program known as the
+              "National Youth Administration"
+            </li>
+            <li>
+              <b>1914</b> - Has to stop school and save up more money. Works in
+              the Civilian Conservation Corps, helping starving Americans. "I
+              saw how food changed them", he said. "All of this left scars on
+              me."
+            </li>
+            <li>
+              <b>1914</b> - Finishes university and takes a job in the US
+              Forestry Service
+            </li>
+            <li>
+              <b>1914</b> - Born in Cresco, Iowa
+            </li>
+            <li>
+              <b>1914</b> - Born in Cresco, Iowa
+            </li>
+            <li>
+              <b>1914</b> - Born in Cresco, Iowa
+            </li>
+            <li>
+              <b>1914</b> - Born in Cresco, Iowa
+            </li>
+          </ul>
+          <div id="testimonial">
+            <p className="prime-minister-testimonial">
+              <i>
+                "Borlaug's life and achievement are testimony to the
+                far-reaching contribution that one man's towering intellect,
+                persistence and scientific vision can make to human peace and
+                progress."
+              </i>
+            </p>
+
+            <p className="prime-minister">
+              <i>-- Indian Prime Minister Manmohan Singh</i>
+            </p>
+          </div>
+          <h3>
+            If you have time, you should read more about this incredible human
+            being on his{" "}
+            <a
+              id="tribute-link"
+              href="https://en.wikipedia.org/wiki/Norman_Borlaug"
+              target="_blank"
+            >
+              Wikipedia entry
+            </a>
+          </h3>
+        </section>
       </header>
     </div>
   );
