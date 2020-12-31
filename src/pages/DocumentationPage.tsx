a13c0c8df2212425f101d3e29a33fe1dbb72283d
Bhavin789 <bhavib.789@gmail.com>
2020-12-31 22:45:11 +0530

    css: Add basic css for the doc layout, WIP

--- src/pages/DocumentationPage.tsx
+++ src/pages/DocumentationPage.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import "../css/survey.css";
+import "../css/documentation.css";
 
 const DocumentationPage = () => {
-  return <div id="main" className="survey"></div>;
+  return (
+    <div id="main" className="documentation">
+      <div className="nav">
+        <div>JS Documentation</div>
+        <div>JS Documentation</div>
+
+        <div>JS Documentation</div>
+        <div>JS Documentation</div>
+        <div>JS Documentation</div>
+        <div>JS Documentation</div>
+      </div>
+
+      <div className="content"> Hello</div>
+    </div>
+  );
 };
 
 export default DocumentationPage;
