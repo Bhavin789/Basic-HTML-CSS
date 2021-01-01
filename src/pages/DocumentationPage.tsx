5835a662b511bac29c7ac95ce30901612d63a0d5
Bhavin789 <bhavib.789@gmail.com>
2021-01-01 13:28:10 +0530

    update: Update HTML elements to resemble more to the semantics, WIP

--- src/pages/DocumentationPage.tsx
+++ src/pages/DocumentationPage.tsx
@@ -3,16 +3,25 @@ import "../css/documentation.css";
 
 const DocumentationPage = () => {
   return (
-    <div id="main" className="documentation">
-      <div className="nav">
-        <div>JS Documentation</div>
-        <div>JS Documentation</div>
-
-        <div>JS Documentation</div>
-        <div>JS Documentation</div>
-        <div>JS Documentation</div>
-        <div>JS Documentation</div>
-      </div>
+    <div id="main-doc">
+      <nav id="navbar">
+        <header className="nav-link header">JS Documentation</header>
+        <a href="#" className="nav-link">
+          JS Documentation
+        </a>
+        <a href="#" className="nav-link">
+          JS Documentation
+        </a>
+        <a href="#" className="nav-link">
+          JS Documentation
+        </a>
+        <a href="#" className="nav-link">
+          JS Documentation
+        </a>
+        <a href="#" className="nav-link">
+          JS Documentation
+        </a>
+      </nav>
 
       <div className="content"> Hello</div>
     </div>
